902e7dd73dcf0dd710bae52595858ff30a157484
Pramod prakash jadhav <pramodj551@gmail.com>
2026-07-15 14:13:35 +0530

    Create __init__.py
    git add src/__init__.py
    
    git commit -m "Add package initialization"

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+"""
+Employee Attrition Data Engineering
+
+Source Package
+
+This package contains all modules required for
+data loading, preprocessing, feature engineering,
+database integration, and exploratory data analysis.
+"""
+
+__version__ = "1.0.0"
+
+__author__ = "Pramod Prakash Jadhav"
+
+__email__ = "pramodj551@gmail.com"
+
+__license__ = "MIT"
